eabf24a80431d7edd2d80283c10bfe4238cbb402
janl <janl99@163.com>
2016-04-05 14:10:42 +0800

    fix bootstrap.sh and add Vundle.vom plug and colorsbox colorscheme project to submodule.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-git pull origin master;
+#pull the laster version 
+#echo "please wait to pull the laster version dotfiles."
+#git pull origin master;
 
-cp .bash_aliases ~/.bash_aliases
-cp .gitattributes ~/.gitattributes
-cp .gitconfig ~/.gitconfig
-cp .gitignore ~/.gitignore
-cp .gvimrc ~/.gvimrc
-cp .vimrc ~/.vimrc
+echo "please wait copying .aliases file to ~/ dir."
+cp -l .bash_aliases ~/.bash_aliases
+
+echo "please wait copying .gitconfig file to ~/ dir."
+cp -l .gitconfig ~/.gitconfig
+
+echo "please wait copying .gitignore file to ~/ dir."
+cp -l .gitignore ~/.gitignore
+
+echo "please wait copying .vim files to ~/ dir."
 cp -r .vim/ ~/.vim
+
+echo "please wait copying vim colorscheme filess to ~/.vim/colors/ dir."
+cp -l vim_colorschemes/colorsbox/colors/colorsbox-material.vim ~/.vim/colors/colorsbox-material.vim 
+
+echo "please wait copying .gvimrc file to ~/ dir."
+cp .gvimrc ~/.gvimrc
+
+echo "please wait copying .vimrc file to ~/ dir."
+cp .vimrc ~/.vimrc
 
