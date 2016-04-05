f5b50476ffb8ab00c36e577019c3c25ccfe41460
janl <janl99@163.com>
2016-04-05 15:57:51 +0800

    fix bootstrap.sh to init dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,11 +13,17 @@ cp -l .gitconfig ~/.gitconfig
 echo "please wait copying .gitignore file to ~/ dir."
 cp -l .gitignore ~/.gitignore
 
-echo "please wait copying .vim files to ~/ dir."
-cp -r .vim/ ~/.vim
+echo "please wait Creating .vim and sub folders. " 
+mkdir ~/.vim/colors;
+mkdir ~/.vim/bundle;
+mkdir ~/.vim/tmp/backup;
+mkdir ~/.vom/tmp/swap;
 
 echo "please wait copying vim colorscheme filess to ~/.vim/colors/ dir."
 cp -l vim_colorschemes/colorsbox/colors/colorsbox-material.vim ~/.vim/colors/colorsbox-material.vim 
+
+echo "please wait copying vom plugin Vundle.vim."
+cp -r vim_plugins/Vundle.vim ~/.vim/bundle/Vundle.vim;
 
 echo "please wait copying .gvimrc file to ~/ dir."
 cp .gvimrc ~/.gvimrc
@@ -25,3 +31,8 @@ cp .gvimrc ~/.gvimrc
 echo "please wait copying .vimrc file to ~/ dir."
 cp .vimrc ~/.vimrc
 
+echo "please wait to install vim plugins. "
+vim +PluginInstall +qall
+
+echo "please wait recuore .bashrc to config file to use."
+source .bashrc
